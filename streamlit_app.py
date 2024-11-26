e4fd439bfa30ce71f7c5223e30c1fd6804f8a6f8
Hjemmeseier <50299192+Hjemmeseier@users.noreply.github.com>
2024-11-26 19:52:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
-import streamlit as st
+mport streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
 # App Title
 st.title("Simple Supported Beam Analysis")
 
+# Inputs
+length = st.number_input("Length of Beam (m):", min_value=0.01, value=5.00, step=0.01, format="%.2f")
+uniform_load = st.number_input("Uniform Load (kN/m):", min_value=0.01, value=10.00, step=0.01, format="%.2f")
